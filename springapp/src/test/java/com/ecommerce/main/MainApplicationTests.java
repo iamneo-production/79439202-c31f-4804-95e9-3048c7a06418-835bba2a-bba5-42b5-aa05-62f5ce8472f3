f2203569e3cf4ff95a9ab143bebe03d35f4ef2e9
SpectureRam <ramphilo@gmail.com>
2023-10-03 17:21:58 +0000

    Committed To Master Branch

--- springapp/src/test/java/com/ecommerce/main/MainApplicationTests.java
+++ springapp/src/test/java/com/ecommerce/main/MainApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.springapp;
+package com.ecommerce.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringappApplicationTests {
+class MainApplicationTests {
 
 	@Test
 	void contextLoads() {
